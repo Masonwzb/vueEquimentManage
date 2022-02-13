a942729352e438b43a11731c753d1878dc5111ef
苏晓薇 <504676706@qq.com>
2022-02-14 01:26:25 +0800

    fix:解决冲突调整

--- src/router/index.js
+++ src/router/index.js
@@ -76,30 +76,29 @@ export const constantRoutes = [{
   component: Layout,
   redirect: '/example/table',
   name: 'Example',
-  meta: { title: 'Example', icon: 'el-icon-s-help' },
-  children: [
-    {
-      path: 'table',
-      name: 'Table',
-      component: () => import('@/views/table/index'),
-      meta: { title: 'Table', icon: 'table' }
-    },
-    {
-      path: 'tree',
-      name: 'Tree',
-      component: () => import('@/views/tree/index'),
-      meta: { title: 'Tree', icon: 'tree' }
-    }
-  ]
-},
-{
-  path: 'tree',
-  name: 'Tree',
-  component: () => import('@/views/tree/index'),
   meta: {
-    title: 'Tree',
-    icon: 'tree'
+    title: 'Example',
+    icon: 'el-icon-s-help'
+  },
+  children: [{
+    path: 'table',
+    name: 'Table',
+    component: () => import('@/views/table/index'),
+    meta: {
+      title: 'Table',
+      icon: 'table'
+    }
+  },
+  {
+    path: 'tree',
+    name: 'Tree',
+    component: () => import('@/views/tree/index'),
+    meta: {
+      title: 'Tree',
+      icon: 'tree'
+    }
   }
+  ]
 },
 
 {
@@ -115,7 +114,6 @@ export const constantRoutes = [{
     }
   }]
 },
-
 {
   path: '/comment',
   component: Layout,
